8c56cd6b6dc294de1400befdfc03a15720162f17
Felix Schaffernicht <f.schaffernicht@syzygy.de>
2017-01-20 14:34:13 +0100

    add basic command line interaction

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 import { computeReview } from './src/compute'
+import prompt from 'prompt'
 
 const names = ['Stephan', 'Dominik', 'Florian', 'Felix']
 
-const pairs = computeReview(names).map(x => `${x.reviewer} => ${x.reviewee}`)
+var schema = {
+    properties: {
+      isMissing: {
+        pattern: /^[a-zA-Z\s\-]+$/,
+        message: 'who is missing today?',
+      }
+    }
+  };
 
-console.log(pairs);
+prompt.start();
+prompt.get(schema, (err, result) => {
+
+  if (err) {
+    console.log('there is a error in your application');
+  }
+
+  console.log('  person: ' + result.isMissing);
+
+  const filteredNames = names.filter(x => x.toLowerCase() !== result.isMissing.toLowerCase())
+
+
+
+  const pairs = computeReview(filteredNames).map(x => `${x.reviewer} => ${x.reviewee}`)
+  console.log(pairs);
+});
